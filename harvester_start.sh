d68fcb8c3b0aff0229e0be391f9da1826ff2a607
Handson-peng <han-sheng.peng@cern.ch>
2020-02-24 09:22:19 +0000

    modify start script

--- harvester_start.sh
+++ harvester_start.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql' --port=3306 --nowatch
-sleep 3
-mysql -e "create database harvester;" && \
-mysql -e "CREATE USER 'harvester'@'localhost' IDENTIFIED BY 'PASSWORD'" && \
-mysql -e "GRANT ALL PRIVILEGES ON harvester.* TO 'harvester'@'localhost';"
+for i in {1..10}
+do
+    if [ -f /var/lib/mysql/`hostname`.pid ]; then
+        mysql -e "create database harvester;" && \
+        mysql -e "CREATE USER 'harvester'@'localhost' IDENTIFIED BY 'PASSWORD'" && \
+        mysql -e "GRANT ALL PRIVILEGES ON harvester.* TO 'harvester'@'localhost';"
+        break
+    else
+        sleep 1
+        continue
+    fi
+done
 /usr/etc/rc.d/init.d/panda_harvester-uwsgi start
+/bin/sh
